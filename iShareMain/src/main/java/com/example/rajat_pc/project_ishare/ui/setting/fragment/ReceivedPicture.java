2b5ca820193f0f53a2c5f0cb058fe3207e49ba45
Rajat Dubey <rajatdubey538@gmail.com>
2017-03-23 11:19:07 +0530

    iShare App

--- iShareMain/src/main/java/com/example/rajat_pc/project_ishare/ui/setting/fragment/ReceivedPicture.java
+++ iShareMain/src/main/java/com/example/rajat_pc/project_ishare/ui/setting/fragment/ReceivedPicture.java
@@ -29,10 +29,9 @@ public class ReceivedPicture extends Fragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_received, container, false);
+        return inflater.inflate(R.layout.fragment_received_picture, container, false);
     }
 
     @Override
